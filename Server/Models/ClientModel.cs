25863039b76a4f72fb7dae2ed5c72a944cb33c45
Dawid Dyrcz <dyrcu15@gmail.com>
2016-07-24 10:34:19 +0200

    Create server simply view with mocked data

---
+++ Server/Models/ClientModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace Server.Models
+{
+    internal class ClientModel
+    {
+        public string ComputerName { get; set; }
+
+        public Socket Socket { get; set; }
+    }
+}
---
+++ Server/ViewModels/ServerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Server.Models;
+using System.Collections.Generic;
+
+namespace Server.ViewModels
+{
+    internal class ServerViewModel : BaseViewModel
+    {
+        private List<ClientModel> _clients;
+        private ClientModel _selectedClient;
+
+        public ServerViewModel()
+        {
+            Clients = new List<ClientModel>()
+            {
+                new ClientModel() { ComputerName = "ddyrcz" },
+                new ClientModel() { ComputerName = "OSKAR" },
+                new ClientModel() { ComputerName = "MARKUS" },
+                new ClientModel() { ComputerName = "SERVER" },
+            };
+        }
+
+        public List<ClientModel> Clients
+        {
+            get { return _clients; }
+            set
+            {
+                _clients = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public ClientModel SelectedClient
+        {
+            get { return _selectedClient; }
+            set
+            {
+                _selectedClient = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
